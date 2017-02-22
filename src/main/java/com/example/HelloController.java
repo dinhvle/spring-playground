df7307e1df693273eea1954f57a8f53346d4120b
Dinh Le <dinh.v.le@gmail.com>
2017-02-22 13:48:20 -0600

    add endpoints to playground

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,16 @@ public class HelloController {
     @GetMapping("/")
     public String helloWorld() {
         return "Hello from Spring!";
+    }
+
+    @PatchMapping("/")
+    public String patchRoute() {
+        return "PUT to index";
+    }
+
+    @DeleteMapping("/")
+    public String deleteRoute(){
+        return "DELETE to index";
     }
 
 }
--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -2,15 +2,46 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(HelloController.class)
 public class DemoApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	public void contextLoads() {
+	public void testHelloEndpoint() throws Exception {
+		this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello from Spring!"));
+    }
+	
+
+	@Test
+	public void testPatchEndpoint() throws Exception {
+		this.mvc.perform(patch("/").accept(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk())
+				.andExpect(content().string("PUT to index"));
+
+	}
+
+	@Test
+	public void testDeleteEndpoint() throws Exception {
+		this.mvc.perform(delete("/").accept(MediaType.TEXT_PLAIN))
+				.andExpect(status().isOk())
+				.andExpect(content().string("DELETE to index"));
 	}
 
 }
